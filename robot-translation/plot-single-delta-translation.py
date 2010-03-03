9cb08986d4074bba0b49294a912259af8aa4d941
Md Omar Faruque Sarker <Mdomarfaruque.Sarker@newport.ac.uk>
2010-03-03 17:50:24 +0000

    Added single robot

---
+++ robot-translation/plot-single-delta-translation.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import sys
+import fileinput
+from numpy import *
+from pylab import *
+
+HEADER_LINE_END = 2
+
+step = []
+du_m = []
+
+if __name__ == '__main__':
+    numargs = len(sys.argv)
+    if numargs < 2:
+        print "Usage: %s  <infile> <outfile>" %sys.argv[0]
+        sys.exit(1)
+    else:
+        infile = sys.argv[1]
+        outfile = sys.argv[2]
+        for line in fileinput.input(infile):
+            if fileinput.lineno() <= HEADER_LINE_END:
+                continue
+            s = int(line.split(';')[1])
+            m = float(line.split(';')[2])
+            step.append(s)
+            du_m.append(m)
+  
+        fileinput.close()   
+        x = arange(len(du_m))
+        y = array(du_m)
+
+        #print y
+        #errorbar(x, y, yerr=du_se, ecolor = '#C0C0C0')
+        plot(x, y)
+
+        xlabel('Time Step (s)')
+        ylabel('Change of Robot Translation (pixel)')
+        #title('Sum of robot translation changes over time ')
+        grid(True)
+        savefig(outfile)
+        show()   
